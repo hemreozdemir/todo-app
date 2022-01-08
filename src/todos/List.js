f48268cdcc099c12488f0b5b6ad72c25b0de2100
hemreozdemirr <88336174+hemreozdemirr@users.noreply.github.com>
2022-01-08 16:29:00 +0300

    files rearrange
    files rearrange

--- src/todos/List.js
+++ src/todos/List.js
@@ -1,28 +1,88 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
+// ** Componenets
+import AddTodo from "./AddTodo";
 import ListItem from "./ListItem";
 
-const List = ({ allTodos, handleChangeTodoChecked, handleRemoveTodo }) => {
+// ** 3rd party components
+import { Card, CardBody, Input } from "reactstrap";
+
+// ** Style
+import "../css/todo.css";
+
+// ** Store
+import { saveTodos, getTodos } from "../data/LocaleDataSource";
+
+const TodoList = () => {
+    const [allTodos, setAllTodos] = useState([]);
+
     useEffect(() => {
-        console.log("1234567890*");
+        if (JSON.parse(getTodos())) {
+            setAllTodos(JSON.parse(getTodos()));
+        }
+    }, []);
+
+    useEffect(() => {
+        saveTodos(JSON.stringify(allTodos));
+        console.log("todo listesi değişti");
     }, [allTodos]);
+
+    const handleSubmitTodo = (todoText) => {
+        let todoId = allTodos.length === 0 ? 0 : allTodos.at(-1).id + 1;
+        const newTodo = {
+            id: todoId,
+            checked: false,
+            text: todoText
+        };
+        setAllTodos((prev) => [...prev, newTodo]);
+    };
+
+    const handleChangeTodoChecked = (id, checked) => {
+        let allTodosTemp = allTodos;
+        allTodosTemp.forEach((element, index) => {
+            if (element.id === id) {
+                allTodosTemp[index].checked = checked;
+            }
+        });
+        console.log("handleChangeTodoChecked");
+        setAllTodos(allTodosTemp);
+        saveTodos(JSON.stringify(allTodosTemp));
+    };
+
+    const handleRemoveTodo = (id) => {
+        let allTodosTemp = allTodos;
+        allTodosTemp.forEach((element, index) => {
+            if (element.id === id) {
+                allTodosTemp.splice(index, 1);
+            }
+        });
+        setAllTodos([...allTodosTemp]);
+    };
+
     return (
-        <React.Fragment>
-            {allTodos &&
-                allTodos.map((todo) => {
-                    return (
-                        <ListItem
-                            key={todo.id}
-                            id={todo.id}
-                            checked={todo.checked}
-                            todoText={todo.text}
-                            handleChangeTodoChecked={handleChangeTodoChecked}
-                            handleRemoveTodo={handleRemoveTodo}
-                        />
-                    );
-                })}
-        </React.Fragment>
+        <div className="todo-container">
+            <Card className="todo-card">
+                <CardBody className="todo-card-body">
+                    <AddTodo handleSubmitTodo={handleSubmitTodo} />
+                    {allTodos &&
+                        allTodos.map((todo) => {
+                            return (
+                                <ListItem
+                                    key={todo.id}
+                                    id={todo.id}
+                                    checked={todo.checked}
+                                    todoText={todo.text}
+                                    handleChangeTodoChecked={
+                                        handleChangeTodoChecked
+                                    }
+                                    handleRemoveTodo={handleRemoveTodo}
+                                />
+                            );
+                        })}
+                </CardBody>
+            </Card>
+        </div>
     );
 };
 
-export default List;
+export default TodoList;
